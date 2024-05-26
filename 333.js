54332528703b09b9795cc835835a0f0363742d2d
alitahi4388 <163639372+alitahi4388@users.noreply.github.com>
2024-05-26 07:32:58 -0700

    Add files via upload

---
+++ 333.js
@@ -0,0 +1,15 @@
+let x=10;
+function increae(x)
+{
+    x++;
+}
+console.log(x);
+//10
+
+let y= { value: 10};
+function increaseobj(y){
+    y.value++
+}
+increaseobj(y);
+console.log(y.value);
+//11
